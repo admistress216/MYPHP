4e9fe4943bb047dc3551fb50a43e7f2cf55136e1
lizengcai <lizengcai@appublisher.com>
2017-06-04 14:43:51 +0800

    框架增加初始化方法__init()

--- MYPHP/Lib/Core/Controller.class.php
+++ MYPHP/Lib/Core/Controller.class.php
@@ -3,6 +3,12 @@
  *父类
  */
 class Controller {
+    public function __construct() {
+        echo 'parent';
+        if(method_exists($this,'__init')) {
+            $this->__init();
+        }
+    }
     protected function success($msg,$url=NULL,$time=3){
         $url = $url ? "window.location.href='".$url."'" : 'window.history.back(-1)';
         include APP_TPL_PATH.'/success.html';
