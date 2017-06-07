85bdcf28c8cc65301da0226d8cd42987ce529f14
lizengcai <lizengcai@appublisher.com>
2017-06-07 23:30:42 +0800

    添加display和assign方法

--- MYPHP/Lib/Core/Controller.class.php
+++ MYPHP/Lib/Core/Controller.class.php
@@ -3,10 +3,27 @@
  *父类
  */
 class Controller {
+    private $var = array();
     public function __construct() {
         if(method_exists($this,'__init')) {
             $this->__init();
         }
+    }
+    protected function display($tpl=NULL) {
+        if(is_null($tpl)) {
+            $path = APP_TPL_PATH.'/'.CONTROLLER.'/'.ACTION.'.html';
+        } else {
+            $suffix = strrchr($tpl, '.');
+            $tpl = empty($suffix) ? $tpl.'.html' : $tpl;
+            $path = APP_TPL_PATH.'/'.CONTROLLER.'/'.$tpl;
+        }
+
+        if(!is_file($path)) halt($path.'模板文件不存在');
+        extract($this->var);
+        include $path;
+    }
+    protected function assign($var,$value) {
+        $this->var[$var] = $value;
     }
     protected function success($msg,$url=NULL,$time=3){
         $url = $url ? "window.location.href='".$url."'" : 'window.history.back(-1)';
